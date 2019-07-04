f4208f94c339c5a65ad4ac83670b6601fde4a2bc
Craig MacGillivray <craigmacgillivray95@gmail.com>
2019-07-04 21:40:48 +0100

    Fix commit

--- ChatClient/ChatClient.Designer.cs
+++ ChatClient/ChatClient.Designer.cs
@@ -35,13 +35,15 @@ namespace ChatClient
             this.btnSend = new System.Windows.Forms.Button();
             this.txtChat = new System.Windows.Forms.RichTextBox();
             this.txtSend = new System.Windows.Forms.TextBox();
+            this.welcomeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnJoin
             // 
-            this.btnJoin.Location = new System.Drawing.Point(243, 22);
+            this.btnJoin.Location = new System.Drawing.Point(584, 26);
+            this.btnJoin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnJoin.Name = "btnJoin";
-            this.btnJoin.Size = new System.Drawing.Size(75, 20);
+            this.btnJoin.Size = new System.Drawing.Size(100, 25);
             this.btnJoin.TabIndex = 2;
             this.btnJoin.Text = "Join";
             this.btnJoin.UseVisualStyleBackColor = true;
@@ -49,34 +51,39 @@ namespace ChatClient
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(79, 22);
+            this.txtName.Location = new System.Drawing.Point(105, 27);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.MaxLength = 10;
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(158, 20);
+            this.txtName.Size = new System.Drawing.Size(209, 22);
             this.txtName.TabIndex = 1;
             // 
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
-            this.lblUserName.Location = new System.Drawing.Point(12, 25);
+            this.lblUserName.Location = new System.Drawing.Point(16, 31);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(61, 13);
+            this.lblUserName.Size = new System.Drawing.Size(81, 17);
             this.lblUserName.TabIndex = 0;
             this.lblUserName.Text = "User name:";
             // 
             // lstUsers
             // 
             this.lstUsers.FormattingEnabled = true;
-            this.lstUsers.Location = new System.Drawing.Point(438, 60);
+            this.lstUsers.ItemHeight = 16;
+            this.lstUsers.Location = new System.Drawing.Point(396, 60);
+            this.lstUsers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstUsers.Name = "lstUsers";
-            this.lstUsers.Size = new System.Drawing.Size(75, 186);
+            this.lstUsers.Size = new System.Drawing.Size(295, 68);
             this.lstUsers.TabIndex = 0;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(438, 256);
+            this.btnSend.Location = new System.Drawing.Point(584, 315);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 55);
+            this.btnSend.Size = new System.Drawing.Size(100, 68);
             this.btnSend.TabIndex = 2;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -84,27 +91,42 @@ namespace ChatClient
             // 
             // txtChat
             // 
-            this.txtChat.Location = new System.Drawing.Point(12, 60);
+            this.txtChat.Location = new System.Drawing.Point(16, 143);
+            this.txtChat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtChat.Name = "txtChat";
             this.txtChat.ReadOnly = true;
-            this.txtChat.Size = new System.Drawing.Size(408, 190);
+            this.txtChat.Size = new System.Drawing.Size(675, 164);
             this.txtChat.TabIndex = 0;
             this.txtChat.Text = "";
             // 
             // txtSend
             // 
-            this.txtSend.Location = new System.Drawing.Point(12, 256);
+            this.txtSend.Location = new System.Drawing.Point(19, 315);
+            this.txtSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSend.Multiline = true;
             this.txtSend.Name = "txtSend";
-            this.txtSend.Size = new System.Drawing.Size(408, 55);
+            this.txtSend.Size = new System.Drawing.Size(543, 67);
             this.txtSend.TabIndex = 1;
             this.txtSend.TextChanged += new System.EventHandler(this.txtSend_TextChanged);
             // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.4F);
+            this.welcomeLabel.Location = new System.Drawing.Point(16, 60);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.welcomeLabel.Size = new System.Drawing.Size(373, 68);
+            this.welcomeLabel.TabIndex = 3;
+            this.welcomeLabel.Text = "Welcome! Enter a username and click join. Online users will show to the right of " +
+    "this text, and messages appear below.\r\nSimply enter a message and click send. Sa" +
+    "y hello!\r\n";
+            // 
             // ChatClient
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 323);
+            this.ClientSize = new System.Drawing.Size(704, 398);
+            this.Controls.Add(this.welcomeLabel);
             this.Controls.Add(this.lstUsers);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txtChat);
@@ -113,6 +135,7 @@ namespace ChatClient
             this.Controls.Add(this.txtSend);
             this.Controls.Add(this.lblUserName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "ChatClient";
             this.Text = "ChatClient";
@@ -130,6 +153,7 @@ namespace ChatClient
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.TextBox txtSend;
         private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.Label welcomeLabel;
     }
 }
 
